7207b8a6eb69064138c6e1da90393ce3751a3bb8
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-05-26 15:16:48 +0600

    Metrics count

--- src/main/java/com/dxc/poc/beam/pipeline/PnrGsToBqPipeline.java
+++ src/main/java/com/dxc/poc/beam/pipeline/PnrGsToBqPipeline.java
@@ -1,6 +1,7 @@
 package com.dxc.poc.beam.pipeline;
 
 import com.dxc.poc.beam.dto.Pnr;
+import com.dxc.poc.beam.utils.logging.CloudLogger;
 import lombok.val;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.SerializableCoder;
@@ -9,53 +10,51 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.metrics.MetricNameFilter;
 import org.apache.beam.sdk.metrics.MetricResult;
-import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.metrics.MetricsFilter;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Iterator;
+import org.apache.beam.sdk.values.KV;
 
 public class PnrGsToBqPipeline {
 
-    private static final Logger logger = LoggerFactory.getLogger(PnrGsToBqPipeline.class);
+    private static final CloudLogger logger = CloudLogger.getLogger(PnrGsToBqPipeline.class);
 
     public static void createAndRunPipeline(GsToBqOptions options) {
         val tableRef = BqMetadataFactory.createTableReference(
-                options.getProject(), options.getDataset(), options.getTableName());
+            options.getProject(), options.getDataset(), options.getTableName());
         val schema = BqMetadataFactory.createTableSchema();
 
         val result = Pipeline.create(options)
-                .apply("Read JSON from file",
-                        TextIO.read().from(options.getInputFile()))
-                .apply("Parse JSON to DTO",
-                        ParseJsons.of(Pnr.class))
-                .setCoder(SerializableCoder.of(Pnr.class))
-                .apply("Convert to table row",
-                        ParDo.of(new ToTableRowDoFn()))
-                .apply("Write to BQ", BigQueryIO.writeTableRows()
-                        .to(tableRef)
-                        .withSchema(schema)
-                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
-                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
-                        .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(options.getGcpTempLocation())))
-                .getPipeline()
-                .run();
+            .apply("Read JSON from file",
+                TextIO.read().from(options.getInputFile()))
+            .apply("Parse JSON to DTO",
+                ParseJsons.of(Pnr.class))
+            .setCoder(SerializableCoder.of(Pnr.class))
+            .apply("Convert to table row",
+                ParDo.of(new ToTableRowDoFn()))
+            .apply("Write to BQ", BigQueryIO.writeTableRows()
+                .to(tableRef)
+                .withSchema(schema)
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                .withCustomGcsTempLocation(ValueProvider.StaticValueProvider.of(options.getGcpTempLocation())))
+            .getPipeline()
+            .run();
 
         result.waitUntilFinish();
 
         val metrics = result.metrics();
 
+        // Write all successful processed
         val metricsResults = metrics.queryMetrics(MetricsFilter.builder()
-                .addNameFilter(MetricNameFilter.named("beam-example", "good_record_count")).build());
+            .addNameFilter(MetricNameFilter.named("beam-example", "good_record_count")).build());
 
-        Iterator<MetricResult<Long>> counters = metricsResults.getCounters().iterator();
-
-        if(counters.hasNext()){
-            val count = counters.next().getAttempted();
-            logger.info("Number of conversions to table: " + count);
+        for (MetricResult<Long> counter : metricsResults.getCounters()) {
+            // Here is only one counter
+            val count = counter.getAttempted();
+            logger.info("Number of good records: " + count,
+                KV.of("stats", "good_records_processed"),
+                KV.of("count", count));
         }
     }
 }
