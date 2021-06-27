84b4bc1426cd6d40c874073b3549935d28574861
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-27 13:36:50 +0600

    Remove old logging

--- src/main/java/com/dxc/poc/beam/GsToBqApplication.java
+++ src/main/java/com/dxc/poc/beam/GsToBqApplication.java
@@ -2,7 +2,6 @@ package com.dxc.poc.beam;
 
 import com.dxc.poc.beam.pipeline.GsToBqOptions;
 import com.dxc.poc.beam.pipeline.PnrGsToBqPipeline;
-import com.dxc.poc.beam.utils.logging.LogContext;
 import com.sabre.gcp.logging.CloudLogger;
 import lombok.val;
 import org.apache.beam.runners.dataflow.DataflowRunner;
@@ -10,39 +9,16 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.values.KV;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class GsToBqApplication {
 
-    // Here is an example of Logback and Slf4j usage
-    private static final Logger log = LoggerFactory.getLogger(GsToBqApplication.class);
-    // Here is an example of wrapper usage
-    private static final CloudLogger cloudLogger = CloudLogger.getLogger(GsToBqApplication.class,
-                                                                KV.of("application-name", "beam-example"),
-                                                                KV.of("job-name", "dxc-stream-demo-job"));
+    private static final CloudLogger LOG = CloudLogger.getLogger(GsToBqApplication.class,
+        KV.of("application-name", "beam-example"),
+        KV.of("job-name", "dxc-stream-demo-job"));
 
     public static void main(String[] args) {
-
-        // Here is an example of info logging in main thread (Wrapper)
-        cloudLogger.info("Entering the Application");
-
-        // Here is an example of manual adding JSON parameters to the context (using Slf4j and Logback)
-        LogContext.setLabel("custom_label", "custom_value");
-        LogContext.setJsonVar("custom_json_var", "custom_json_value");
-        LogContext.setJsonVar("message", "This is a message injected to JSON");
-        log.info("Starting the GS2BQ Example pipeline 1...");
-        LogContext.clearJsonVar();
-
-        // another message
-        LogContext.setLabel("custom_label", "custom_value_new");
-        LogContext.setLabel("custom_label_2", "custom_value_new_2");
-
-        // Adding custom labels using enhancer (Logback and Slf4j)
-        log.info("label1=value1|Starting the GS2BQ Example pipeline 2...");
-        LogContext.clearJsonVar();
-        log.error("label2=value2|label2=value2|Custom Labels example");
+        LOG.info("Entering the Application", KV.of("1", "2"));
 
         val options = PipelineOptionsFactory.fromArgs(args)
             .withValidation()
@@ -50,7 +26,7 @@ public class GsToBqApplication {
         options.setRunner(DataflowRunner.class);
 
         if (options.getTemplateLocation() != null) {
-            log.info("Call for a template generation");
+            LOG.info("Call for a template generation");
             try {
                 PipelineResult result = Pipeline.create(options).run();
                 result.getState();
@@ -60,13 +36,13 @@ public class GsToBqApplication {
                 // This is workaround for https://issues.apache.org/jira/browse/BEAM-9337
             } catch (Exception e) {
                 // Here is an example of error logging (Logback + Slf4j)
-                log.error("Unexpected error", e);
+                LOG.error("Unexpected error", e);
             }
         } else {
-            log.info("Start processing");
+            LOG.info("Start processing");
             PnrGsToBqPipeline.createAndRunPipeline(options);
         }
 
-        log.info("GS2BQ processing finished.");
+        LOG.info("GS2BQ processing finished.");
     }
 }
