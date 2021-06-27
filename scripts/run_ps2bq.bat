62cf44b451729d2072632a59bb6a2bcd6a0e311f
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-27 13:36:21 +0600

    Adding job name to .bat script

--- scripts/run_ps2bq.bat
+++ scripts/run_ps2bq.bat
@@ -2,4 +2,4 @@
 
 call vars.bat
 
-call java -cp ../target/beam-example.jar com.dxc.poc.beam.PubSubToBqApplication --appName=pubsubtobqapplication --project=sabre-cdw-dev-sandbox --region=us-central1 --inputTopic=projects/sabre-cdw-dev-sandbox/topics/%TOPIC_ID% --dataset=beam_example --gcpTempLocation=gs://cdm-apache-beam-example/temp --stagingLocation=gs://cdm-apache-beam-example/staging
+call java -cp ../target/beam-example.jar com.dxc.poc.beam.PubSubToBqApplication --jobName=pub_sub_to_bq --project=sabre-cdw-dev-sandbox --region=us-central1 --inputTopic=projects/sabre-cdw-dev-sandbox/topics/%TOPIC_ID% --dataset=beam_example --gcpTempLocation=gs://cdm-apache-beam-example/temp --stagingLocation=gs://cdm-apache-beam-example/staging
