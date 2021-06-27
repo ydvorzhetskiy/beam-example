6bf708644e54d035b7b12327bb012b4811cc55b1
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-06-27 13:25:04 +0600

    Add .bat scripts

---
+++ scripts/publish.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call vars.bat
+
+call gcloud scheduler jobs create pubsub %JOB_NAME% --schedule="* * * * *" --topic=%TOPIC_ID% --message-body="{\"pr_locator_id\":\"FGDNKU\",\"ticket_number\": \"1c345a7812u45\", \"pr_create_date\": \"2021-05-11\", \"pr_sequence\": 1, \"from_datetime\": \"2021-05-11 13:35:12\", \"tr_datetime\": \"2021-05-11 13:35:12\", \"creditcard\": [{ \"issuing_network\": \"Visa\", \"card_number\": \"4485764219977839\"}]}"
+
+call gcloud scheduler jobs run %JOB_NAME%
---
+++ scripts/run_ps2bq.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call vars.bat
+
+call java -cp ../target/beam-example.jar com.dxc.poc.beam.PubSubToBqApplication --appName=pubsubtobqapplication --project=sabre-cdw-dev-sandbox --region=us-central1 --inputTopic=projects/sabre-cdw-dev-sandbox/topics/%TOPIC_ID% --dataset=beam_example --gcpTempLocation=gs://cdm-apache-beam-example/temp --stagingLocation=gs://cdm-apache-beam-example/staging
