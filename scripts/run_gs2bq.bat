0fbda2d6edd9288bb4849867c5decfeae29fbd98
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-08-05 05:39:44 +0600

    4 times error

--- scripts/run_gs2bq.bat
+++ scripts/run_gs2bq.bat
@@ -2,4 +2,11 @@
 
 call vars.bat
 
-call java -jar ../target/beam-example.jar --project=sabre-cdw-dev-sandbox --region=us-central1 --jobName=gs-to-bq-job --inputFile=gs://cdm-apache-beam-example/input/in.err.txt --dataset=beam_example --gcpTempLocation=gs://cdm-apache-beam-example/temp --stagingLocation=gs://cdm-apache-beam-example/staging
+call java -jar ../target/beam-example.jar^
+  --project=sabre-cdw-dev-sandbox^
+  --region=us-central1^
+  --jobName=gs-to-bq-job^
+  --inputFile=gs://cdm-apache-beam-example/input/in.long.txt^
+  --dataset=beam_example^
+  --gcpTempLocation=gs://cdm-apache-beam-example/temp^
+  --stagingLocation=gs://cdm-apache-beam-example/staging
--- src/main/java/com/dxc/poc/beam/pipeline/ToTableRowDoFn.java
+++ src/main/java/com/dxc/poc/beam/pipeline/ToTableRowDoFn.java
@@ -12,8 +12,8 @@ import org.apache.beam.sdk.values.KV;
 public class ToTableRowDoFn extends DoFn<Pnr, TableRow> {
 
     private final CloudLogger log = CloudLogger.getLogger(ToTableRowDoFn.class,
-                                                KV.of("application-name", "beam-example"),
-                                                KV.of("job-name", "dxc-stream-demo-job"));
+        KV.of("application-name", "beam-example"),
+        KV.of("job-name", "dxc-stream-demo-job"));
 
     private final Counter goodRecordCounter = Metrics.counter("beam-example", "good_record_count");
     private final Counter badRecordCounter = Metrics.counter("beam-example", "bad_record_count");
@@ -21,22 +21,12 @@ public class ToTableRowDoFn extends DoFn<Pnr, TableRow> {
     @ProcessElement
     public void processElement(@Element Pnr pnr, OutputReceiver<TableRow> out) {
 
-        // Here is an example of debug logging
-        log.debug("PNR will be processed", KV.of("ticket_number", pnr.getTicketNumber()));
-
-        try {
-            val row = PnrConverter.toTableRow(pnr);
-            out.output(row);
-            goodRecordCounter.inc();
-        } catch (NumberFormatException e) {
-            badRecordCounter.inc();
-
-            // Here is an example of error logging
-            log.error("Number format validation log record!",
-                KV.of("error_type", "Numeric_Validation_Error"));
-
-        } catch (Exception e) {
-            log.error("Unexpected exception:", e);
+        val row = PnrConverter.toTableRow(pnr);
+        out.output(row);
+        goodRecordCounter.inc();
+        if (pnr.getTicketNumber().endsWith("1")) {
+            log.error("Repeated error message! " + pnr.getTicketNumber());
+            throw new RuntimeException("Repeated exception!" + pnr.getTicketNumber());
         }
     }
 }
